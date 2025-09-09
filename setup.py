f6a66999fb9ece9abf45d785ba984b7b9f175435
Daniil Alekseev <125923529+Danex-Exe@users.noreply.github.com>
2025-09-09 19:51:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description='The module checks for the presence of a .version file in the local repository, as well as the presence of a remote git source. It then compares the contents of the .version file on the repository and the local one. If there are differences, it executes the git pull command.',
     author='Daniil Alekseev',
     author_email='sevenaspects.mail@gmail.com',
-    version="1.0.0-alpha",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=["requests"],
     python_requires=">=3.8",
